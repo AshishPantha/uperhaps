08d60423eea8ffb40b9d86f40a23d30ab2ec9d95
ftbhabuk <ftraveller23@gmail.com>
2026-03-18 20:29:06 +0545

    feat : database img local upladed!

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
           },
           {
           protocol: "https",
-          hostname: "poetry-web2.onrender.com",
+          hostname: "uperhaps.onrender.com",
         },
       ],
     },
